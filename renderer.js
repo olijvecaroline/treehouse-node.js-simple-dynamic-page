a8f09c05dd3e26610d5463fc2a898b76280b4812
Caroline Olijve <olijvecaroline@gmail.com>
2017-06-28 14:43:06 +0200

    merging views with values added

--- renderer.js
+++ renderer.js
@@ -1,7 +1,16 @@
 var renderer = require("fs");
 
+function mergeValues(values,content){
+  for(var key in values){
+    content = content.replace("{{"+key+"}}",values[key]);
+  }
+  return content;
+}
+
 function view(templateName, values, response){
+  
   var fileContents = fs.readFileSync('./views/' + templateName + '.html');
+  fileContents = mergeValues(values,fileContents)
   response.write(fileContents);
 }
 module.exports.view = view;
